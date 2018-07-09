c11edd1d58291631be852bf51c63e9ebf3cc8cd8
William Kibirango <williamkaos.kibirango76@gmail.com>
2018-07-09 16:40:57 +0300

    managed to search for required columns

--- example-parser.py
+++ example-parser.py
@@ -2,8 +2,32 @@
 
 import csv
 import sys
+import re
 
 file_name = sys.argv[1]
+
+required_columns = [
+  re.compile(r'^(category|type|infection)$'),
+  re.compile(r'^(address|ip)$'),
+  re.compile(r'^asn$'),
+  re.compile(r'^timestamp$'),
+  re.compile(r'^city$'),
+  re.compile(r'^error message^'),
+  re.compile(r'^url^'),
+  re.compile(r'^(http_agent|agent)^'),
+  re.compile(r'^http_host$'),
+  re.compile(r'^(infection|type|category)$'),
+  re.compile(r'^(dst_port|dest_port|cc_port)$'),
+  re.compile(r'^(dst_asn|dest_asn|cc_asn)$'),
+  re.compile(r'^(dest_ip|dst_ip|cc_ip)$'),
+  re.compile(r'^service$'),
+  re.compile(r'^source$'),
+  re.compile(r'^reason$'),
+  re.compile(r'^notes$'),
+  re.compile(r'^src_port$')
+]
+
+col_index = []
 
 with open(file_name) as file_to_process:
   # for standard csv files
@@ -16,9 +40,20 @@ with open(file_name) as file_to_process:
                     quoting = csv.QUOTE_NONNUMERIC,
                     skipinitialspace = True
                     )
+  titles = list(rows)[0]
 
-  for row in rows:
-    print(row)
+  print(titles)
+  
+  for i in range(len(titles)):
+    for col in required_columns:
+      if col.match(titles[i]):
+        col_index.append(i)
+        break
+  
+  print(col_index)
 
+  #for row in rows:
+  #  print(row)
 
+  
 
